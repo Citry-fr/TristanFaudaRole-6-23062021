7da5a381f0adb20edb16583f78e654ecac145235
Tristan <citrynox9@gmail.com>
2021-06-29 22:39:15 +0200

    Commit #21
    Modification :
    
    Ré-installation de bcrypt plus utilisation de body-parser dans app.js
    
    Création :

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const saucesRoutes = require('./routes/sauces');
 const userRoutes = require('./routes/user');
@@ -19,7 +20,9 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-})
+});
+
+app.use(bodyParser.json());
 
 app.use('/api/auth', userRoutes);
 
