3cf5e9f59e3c6093912291a52a5c163556498da5
Tristan <citrynox9@gmail.com>
2021-06-29 22:07:44 +0200

    Commit #19
    Modification :
    
    Modification hiérarchie du code 
    
    Création :

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,6 @@ const mongoose = require('mongoose');
 const saucesRoutes = require('./routes/sauces');
 const userRoutes = require('./routes/user');
 
-const app = express();
-
 mongoose.connect('mongodb+srv://citry:3MMqF0NeaoOltK3q@cluster0.inyvs.mongodb.net/projetOpenClassroom?retryWrites=true&w=majority', 
     { 
         useNewUrlParser: true,
@@ -13,6 +11,15 @@ mongoose.connect('mongodb+srv://citry:3MMqF0NeaoOltK3q@cluster0.inyvs.mongodb.ne
     })
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+})
 
 app.use('/api/auth', userRoutes);
 
