794c59c918fdc98025ea20bf6baf25f53e915a81
Tristan <citrynox9@gmail.com>
2021-07-01 17:46:57 +0200

    Commit #31
    Modification :
    
    Modification multer
    
    Création :

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -9,12 +9,14 @@ exports.getAllSauces = (req, res, next) => {
 };
 
 exports.createSauce = (req, res, next) => {
+    
     const sauceObject = JSON.parse(req.body.sauce);
-    delete sauce._id;
+    delete sauceObject._id;
     const sauce = new Sauce({
         ...sauceObject,
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
+    console.log(sauce);
     sauce.save()
         .then(() => res.status(201).json({ message: 'Post sauvegardé !' }))
         .catch(error => res.status(400).json({ error }));
