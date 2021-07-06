e9cbec7f51d721b91aab9ddd4005a92f7ffd1ac3
Tristan <citrynox9@gmail.com>
2021-07-06 14:11:52 +0200

    Commit #37
    Modification :
    
    Modification erreur typo dans la suppression de l'image 
    
    Création :

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -36,7 +36,7 @@ exports.modifySauce = (req, res, next) => {
 exports.deleteSauce = (req, res, next) => {
     Sauce.findOne({_id: req.params.id})
         .then( sauce => {
-            const filename = sauce.imageUrl.split('/image/')[1];
+            const filename = sauce.imageUrl.split('/images/')[1];
             fs.unlink(`images/${filename}`, () => {
                 Sauce.deleteOne({ _id: req.params.id })
                     .then(() => res.status(200).json({ message: 'Objet Supprimé' }))
